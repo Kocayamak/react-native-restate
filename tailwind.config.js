98ef838c3c9d9d89b16a75f56af25ec2e48ba18b
Kocayamak <mikailkocayamak@gmail.com>
2024-12-28 20:21:37 +0300

    feat : apply use font project.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,33 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        rubik: ["Rubik-regular", "sans-serif"],
+        "Rubik-bold": ["Rubik-Bold", "sans-serif"],
+        "Rubik-medium": ["Rubik-Medium", "sans-serif"],
+        "Rubik-light": ["Rubik-Light", "sans-serif"],
+        "Rubik-extrabold": ["Rubik-ExtraBold", "sans-serif"],
+        "Rubik-semibold": ["Rubik-SemiBold", "sans-serif"],
+      },
+      colors: {
+        primary: {
+          100: "#0061FF0A",
+          200: "#0061FF1A",
+          300: "#0061FF",
+        },
+        accent: {
+          100: "#FBFBFD",
+        },
+        black: {
+          DEFAULT: "#000000",
+          100: "#8C8E98",
+          200: "#666876",
+          300: "#191D31",
+        },
+        danger: "#F75555",
+      },
+    },
   },
   plugins: [],
 };
