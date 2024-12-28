7e5a83d2f017c23f88cc056d1b2b28addc22530c
Kocayamak <mikailkocayamak@gmail.com>
2024-12-28 19:22:14 +0300

    feat : added pages.

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,19 @@
+import { Text, View } from "react-native";
+import {Link} from "expo-router";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+        <Link href="/sign-in" >Kayıt Ol</Link>
+        <Link href="/profile" >Profil</Link>
+        <Link href="/explore" >Keşfet</Link>
+        <Link href="/properties/1">Özellikler</Link>
+    </View>
+  );
+}
